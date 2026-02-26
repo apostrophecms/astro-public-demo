1198222663e924e30add595c6e877c2cfc3231b2
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2026-02-26 17:53:11 +0200

    Add sitemap

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ apostrophe({
   root: import.meta,
   shortName: 'astro-public-demo',
   // Overridden by APOS_BASE_URL
-  // baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://localhost:3000',
+  // The base URL when static building the Astro frontend.
   // Overridden by APOS_STATIC_BASE_URL
   // staticBaseUrl: 'http://static.localhost:3000',
   modules: {
@@ -29,6 +30,9 @@ apostrophe({
 
     // Manage page and piece open graph data
     '@apostrophecms/open-graph': {},
+
+    // Generate XML sitemaps
+    '@apostrophecms/sitemap': {},
 
     // Widgets
     '@apostrophecms/rich-text-widget': {},
